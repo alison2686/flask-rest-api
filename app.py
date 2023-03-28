2e1b23ec4cb41a6a74c8d6f20d296aececfd2749
Alison Lee <alison2686@gmail.com>
2023-03-28 14:15:15 -0700

    new endpoint for REST API

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
+
+## get individual store
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
 ## create new store using post request
 @app.post("/store")
 def create_store():
@@ -26,6 +36,32 @@ def create_store():
     store = {**store_data, "id": store_id}
     stores[store_id] = store
     return store, 201
+
+
+## delete specific item from store
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+
+## get all items in a store
+@app.get("/item")
+def get_all_items():
+    return {"items": list(items.values())}
+
+
+## get specific item from store
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found.")
+
 
 ## add item to store using post request
 @app.post("/item")
@@ -45,7 +81,7 @@ def create_item():
             item_data["name"] == item["name"]
             and item_data["store_id"] == item["store_id"]
         ):
-            abort(400, message=f"Item alredy exisits.")
+            abort(400, message="Item alredy exisits.")
 
     if item_data["store_id"] not in stores:
         abort(404, message="Store not found.")
@@ -56,24 +92,31 @@ def create_item():
 
     return item, 201
 
-## get individual store
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, message="Store not found.")
 
-## get specific item from store
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
+## delete specific item from store
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
     try:
-        return items[item_id]
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
         abort(404, message="Item not found.")
 
-## get all items in a store
-@app.get("/item")
-def get_all_items():
-    return {"items": list(items.values())}
+
+## update item
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included")
+    try:
+        item = items[item_id]
+        item |= item_data
+
+        return item
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+
         
